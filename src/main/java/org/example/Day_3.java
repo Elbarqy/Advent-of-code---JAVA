60a7e7701669e6b1676647e34c8da580fc276e80
Elbarqy <elbarqy.dev@gmail.com>
2024-04-19 12:10:24 +0200

    DAY_3 PART II

--- src/main/java/org/example/Day_3.java
+++ src/main/java/org/example/Day_3.java
@@ -3,37 +3,52 @@ package org.example;
 import org.example.utils.Utils;
 
 import java.io.IOException;
-import java.util.ArrayList;
+import java.util.*;
 
 public class Day_3 {
-    public static boolean isAdjacentToSymbol(String[] arr, int i, int j, int sz, int lineSz) {
-        int[] iPlus = new int[]{0, 0, 1, 1, -1, -1, 1, -1};
-        int[] jPlus = new int[]{1, -1, 1, -1, -1, 1, 0, 0};
-        for (int idx = 0; idx < 8; ++idx) {
+    public static HashMap<String, Integer> getAdjacentNumbers(String[] arr, int i, int j, int sz, int lineSz) {
+        HashMap<String, Integer> result = new HashMap<>();
+        int[] iPlus = new int[]{0, 0, 1, 1, 1, -1, -1, -1};
+        int[] jPlus = new int[]{1, -1, -1, 0, 1, -1, 0, 1};
+        HashMap<Integer, ArrayList<Integer>> indices = new HashMap<>();
+        if (arr[i].charAt(j) == '*') for (int idx = 0; idx < 8; ++idx) {
             int newI = iPlus[idx] + i;
             int newJ = jPlus[idx] + j;
-            if (Character.isDigit(arr[i].charAt(j)) && newI > 0 && newJ > 0 && newI < sz && newJ < lineSz && !Character.isDigit(arr[newI].charAt(newJ)) && arr[newI].charAt(newJ) != '.') {
-                return true;
+            if (newI >= 0 && newJ >= 0 && newI < sz && newJ < lineSz && Character.isDigit(arr[newI].charAt(newJ)) && arr[newI].charAt(newJ) != '.') {
+                int[] boundaries = getNumberBoundariesAt(arr, newI, newJ, lineSz);
+                result.put(
+                        newI + "," + boundaries[0], boundaries[1]
+                );
             }
         }
-        return false;
+        return result;
     }
+
+    public static int[] getNumberBoundariesAt(String[] arr, int i, int j, int lineSz) {
+        int l = j, r = j;
+        while (l > 0 && Character.isDigit(arr[i].charAt(l))) --l;
+        while (r < lineSz - 1 && Character.isDigit(arr[i].charAt(r))) ++r;
+        if (!Character.isDigit(arr[i].charAt(l))) ++l;
+        if (!Character.isDigit(arr[i].charAt(r))) --r;
+        return new int[]{l, r + 1};
+    }
+
+
     public static void main(String[] args) throws IOException {
         ArrayList<String> fs = Utils.readInputLines("gearRatio");
         String[] lines = fs.toArray(new String[0]);
         int sum = 0, sz = lines.length, lineSz = lines[0].length();
         for (int i = 0; i < sz; ++i) {
             for (int j = 0; j < lineSz; ++j) {
-                if (isAdjacentToSymbol(lines, i, j, sz, lineSz)) {
-                    int l = j, r = j;
-                    while (l > 0 && Character.isDigit(lines[i].charAt(l))) --l;
-                    while (r < lineSz - 1 && Character.isDigit(lines[i].charAt(r))) ++r;
-                    if (!Character.isDigit(lines[i].charAt(l))) ++l;
-                    if (!Character.isDigit(lines[i].charAt(r))) --r;
-                    String x = lines[i].substring(l, r + 1);
-                    int adder = Integer.parseInt(x);
-                    sum += adder;
-                    j = r;
+                var result = getAdjacentNumbers(lines, i, j, sz, lineSz);
+                if (result.size() == 2) {
+                    ArrayList<Integer> nums = new ArrayList<>();
+                    for (var entry : result.entrySet()) {
+                        String[] pos = entry.getKey().split(",");
+                        String x = lines[Integer.parseInt(pos[0])].substring(Integer.parseInt(pos[1]), entry.getValue());
+                        nums.add(Integer.parseInt(x));
+                    }
+                    sum += nums.stream().reduce(1, (a, b) -> a * b);
                 }
             }
         }
