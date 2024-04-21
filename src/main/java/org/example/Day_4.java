79d829588a71d35e4f8131ec737c4c41e5f5d370
Elbarqy <elbarqy.dev@gmail.com>
2024-04-21 09:10:01 +0200

    day-4 part II

--- src/main/java/org/example/Day_4.java
+++ src/main/java/org/example/Day_4.java
@@ -8,30 +8,46 @@ import java.util.HashMap;
 import java.util.HashSet;
 
 public class Day_4 {
-    public static int calcPoints(String line) {
-        String match = line.split(":")[1];
+    public static int calcPoints(String line, HashMap<Integer, Integer> copies) {
+        String[] identifiers = line.split(":");
+        String match = identifiers[1];
+        String[] temp = identifiers[0].split(" ");
+        int cardId = Integer.parseInt(temp[temp.length - 1]);
         String[] input = match.split("\\|");
         String[] winning = input[0].split(" ");
         HashSet<Integer> winningCards = new HashSet<>();
         for (String num : winning) {
-            if (num.length() > 0) winningCards.add(Integer.parseInt(num));
+            if (!num.isEmpty()) winningCards.add(Integer.parseInt(num));
         }
-        int sum = 0;
         String[] ownedCards = input[1].split(" ");
+        int count = 0;
         for (String owned : input[1].split(" ")) {
-            if (owned.length() > 0 && winningCards.contains(Integer.parseInt(owned))) {
-                if(sum == 0) sum = 1;
-                else sum <<=1;
+            if (!owned.isEmpty() && winningCards.contains(Integer.parseInt(owned))) {
+                ++count;
             }
         }
-        return sum;
+        int currCopies = copies.getOrDefault(cardId, 0);
+        ++currCopies;
+        copies.put(cardId, currCopies);
+        for (int i = 1; i <= count; ++i) {
+            int val = copies.getOrDefault(cardId + i, 0);
+            copies.put(cardId + i, (currCopies + val));
+        }
+        return cardId;
     }
 
     public static void main(String[] args) throws IOException {
         ArrayList<String> lines = Utils.readInputLines("scratchpaper");
-        int sum = 0;
+        int last = 1;
+        HashMap<Integer, Integer> copies = new HashMap<>();
         for (String line : lines) {
-            sum += calcPoints(line);
+            last = calcPoints(line, copies);
+        }
+        Long sum = 0L;
+        for (var entry : copies.entrySet()) {
+            if (entry.getKey() <= last) {
+                sum += entry.getValue();
+            }
         }
         System.out.println(sum);
     }
