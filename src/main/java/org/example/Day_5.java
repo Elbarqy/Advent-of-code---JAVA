30b58fe695d6dc20e26581672026eb500810821b
Elbarqy <elbarqy.dev@gmail.com>
2024-04-22 13:20:50 +0200

    DAY 5 part II but outOfMemoryError

--- src/main/java/org/example/Day_5.java
+++ src/main/java/org/example/Day_5.java
@@ -4,14 +4,26 @@ import org.example.utils.Utils;
 
 import java.io.*;
 import java.util.*;
-import java.util.concurrent.atomic.AtomicInteger;
-import java.util.concurrent.atomic.AtomicLong;
 
 public class Day_5 {
+    public static long[] getOverlapRange(long[] originalRange, long[] mappedRange) {
+        long mEnd = mappedRange[0] + mappedRange[1] - 1, end = originalRange[0] + originalRange[1] - 1;
+        long overlapStart = Math.max(originalRange[0], mappedRange[0]), overlapEnd = Math.min(mEnd, end);
+        if (overlapStart <= overlapEnd) {
+            return new long[]{overlapStart, overlapEnd - overlapStart + 1};
+        }
+        return null;
+    }
+
     public static void main(String[] args) throws IOException {
         String content = Utils.getResourceFileAsString("inputs/fertilizer");
         var blocks = content.split("\n\n");
         long[] seeds = Arrays.stream(blocks[0].split(":")[1].split(" ")).filter(s -> !Objects.equals(s, "")).mapToLong(Long::parseLong).toArray();
+        long[][] seedRanges = new long[seeds.length / 2][2];
+        for (int i = 0; i < seeds.length / 2; ++i) {
+            int newIdx = i * 2;
+            seedRanges[i] = new long[]{seeds[newIdx], seeds[newIdx + 1]};
+        }
         HashMap<String, List<long[]>> mapper = new HashMap<>();
         Arrays.stream(Arrays.copyOfRange(blocks, 1, blocks.length))
                 .forEach(block -> {
@@ -34,16 +46,45 @@ public class Day_5 {
                 "humidity-to-location"
         };
         long[] min = {Long.MAX_VALUE};
-        Arrays.stream(seeds).forEach(seed -> {
-            AtomicLong currVal = new AtomicLong(seed);
+
+        Stack<long[]> ranges = new Stack<>();
+        Arrays.stream(seedRanges).forEach(seedRange -> {
+            ranges.push(seedRange);
             Arrays.stream(keys).forEach(key -> {
-                ArrayList<long[]> validRange = new ArrayList<>();
-                mapper.get(key).stream().filter(range -> currVal.get() >= range[1] && currVal.get() <= range[1] + range[2]).forEach(validRange::add);
-                if (!validRange.isEmpty()) {
-                    currVal.addAndGet(-1 * validRange.get(0)[1] + validRange.get(0)[0]);
-                }
+                ArrayList<long[]> generatedRanges = new ArrayList<>();
+                mapper.get(key).forEach(mappedRange -> {
+                    while (!ranges.isEmpty()) {
+                        long[] originalRange = ranges.pop();
+                        long[] overlap = getOverlapRange(originalRange, new long[]{mappedRange[1], mappedRange[2]});
+                        if (overlap != null) {
+                            long diff = mappedRange[0] - mappedRange[1];
+                            generatedRanges.add(new long[]{
+                                    overlap[0] + diff, overlap[1]
+                            });
+                            if (originalRange[0] < overlap[0]) {
+                                generatedRanges.add(new long[]{
+                                        originalRange[0],
+                                        overlap[0] - originalRange[0]
+                                });
+                            }
+                            long end = originalRange[0] + originalRange[1] - 1;
+                            if (end > overlap[0] + overlap[1] - 1) {
+                                long temp = overlap[0] + overlap[1];
+                                generatedRanges.add(new long[]{
+                                        overlap[0] + overlap[1], end - temp - 1
+                                });
+                            }
+                        } else {
+                            generatedRanges.add(originalRange);
+                        }
+                    }
+                    ranges.addAll(generatedRanges);
+                });
             });
-            min[0] = Math.min(min[0], currVal.longValue());
+            ranges.forEach(range -> {
+                if (min[0] > range[0]) min[0] = range[0];
+            });
+            ranges.clear();
         });
         System.out.println(min[0]);
     }
